9c033ee78c6ba85c983720ca800dc1e7c5446835
ldaurcvvrk <111478801+ldaurcvvrk@users.noreply.github.com>
2025-03-15 19:52:59 +0800

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -5,10 +5,7 @@
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
-
-set DIRNAME=%~dp0
+@rem Set local scope for the variables with 
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
